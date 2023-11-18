4162b6f711529482c82d69fca4e820ab51586eed
Juliana <jflorez@proton.me>
2023-11-17 22:47:24 -0500

    paginado

--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -1,5 +1,5 @@
 import './Home.css';
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDogs, getbyName } from "../../redux/actions";
 import Navbar from "../../Components/Navbar/Navbar";
@@ -9,21 +9,58 @@ function Home() {
   const dispatch = useDispatch();
   const allDogs = useSelector((state) => state.allDogs);
   const dogsName = useSelector((state) => state.dogsName);
+  const [currentPage, setCurrentPage] = useState(1);
+  const dogsPerPage = 8;
 
   useEffect(() => {
     dispatch(getDogs());
   }, [dispatch]);
 
+  const indexOfLastDog = currentPage * dogsPerPage;
+  const indexOfFirstDog = indexOfLastDog - dogsPerPage;
+  const currentDogs = allDogs.slice(indexOfFirstDog, indexOfLastDog);
+
+  const totalPages = Math.ceil(allDogs.length / dogsPerPage);
+
+  function handlePageChange(newPage) {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  }
+
   return (
     <div className="homecontainer">
+
+
       <Navbar />
+
+      <div className="pagination">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Anterior
+        </button>
+        <h3>{currentPage} / {totalPages} </h3>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Siguiente
+        </button>
+      </div>
 
       {dogsName && dogsName.length > 0 ? (
         <Cards allDogs={dogsName} />
       ) : (
-        <Cards allDogs={allDogs} />
+        <Cards allDogs={currentDogs} />
       )}
+
+
+
     </div>
+    
+
   );
 }
 
